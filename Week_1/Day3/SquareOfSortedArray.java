e3ec906054ad64f3d5993872213e4b709ceaaf78
mr_avnish <ak1789511@gmail.com>
2026-06-03 20:53:42 +0530

    Day 3

---
+++ Week_1/Day3/SquareOfSortedArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SquareOfSortedArray {
+     public int[] sortedSquares(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] * arr[i];
+    }
+    Arrays.sort(arr);
+    return arr;
+    }
+}
+
