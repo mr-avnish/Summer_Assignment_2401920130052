d9a9360dc2563fdd16241c79d9267dedd39c7e7c
mr_avnish <ak1789511@gmail.com>
2026-06-18 14:45:06 +0530

    Day 16

---
+++ Week3/Day4/EvaluateReversePolishNotation.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+      public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (String s : tokens) {
+
+            if (s.equals("+")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a + b);
+            }
+            else if (s.equals("-")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            }
+            else if (s.equals("*")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a * b);
+            }
+            else if (s.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            }
+            else {
+                stack.push(Integer.parseInt(s));
+            }
+        }
+
+        return stack.pop();
+    }
+}
