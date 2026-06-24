900bd8b0b012503db16e2799864017709fc85bc3
mr_avnish <ak1789511@gmail.com>
2026-06-24 18:25:37 +0530

    Day 21

---
+++ Week4/Day3/LowestCommonAncestorofaBST.java
@@ -0,0 +1,22 @@
+import javax.swing.tree.TreeNode;
+
+public class LowestCommonAncestorofaBST {
+    
+     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p,TreeNode q) {
+    
+    while (root != null) {
+        if (p.val < root.val && q.val < root.val) {
+            root = root.left;
+        } 
+        else if (p.val > root.val && q.val > root.val) {
+            root = root.right;
+        } 
+        else {
+            return root;
+        }
+    }
+    return null;
+    }
+}
+
+
